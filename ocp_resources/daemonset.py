b141d42508dac6927093f923a1c49fb37f573c8a
Sarah Bennert <sbennert@redhat.com>
2022-03-29 09:28:36 -0400

    [WIP] Update daemonset wait_ methods

--- ocp_resources/daemonset.py
+++ ocp_resources/daemonset.py
@@ -26,23 +26,54 @@ class DaemonSet(NamespacedResource):
         Raises:
             TimeoutExpiredError: If not all the pods are deployed.
         """
-        LOGGER.info(f"Wait for {self.kind} {self.name} to deploy all desired pods")
+        self.wait_for_pods(scheduled=True, timeout=timeout)
+
+    def wait_for_pods(self, scheduled=True, timeout=TIMEOUT_4MINUTES):
+        """
+        Wait until all pods are updated.
+
+        Args:
+            scheduled (bool): True for pods scheduled, False for not scheduled.
+            timeout (int): Time to wait for the daemonset.
+
+        Raises:
+            TimeoutExpiredError: If conditions do not match within timeout
+        """
+        LOGGER.info(f"Wait for {self.kind} {self.name} to be scheduled: {scheduled}")
         samples = TimeoutSampler(
             wait_timeout=timeout,
             sleep=1,
             exceptions_dict=PROTOCOL_ERROR_EXCEPTION_DICT,
             func=self.api.get,
             field_selector=f"metadata.name=={self.name}",
-            namespace=self.namespace,
         )
         for sample in samples:
             if sample.items:
-                status = sample.items[0].status
-                desired_number_scheduled = status.desiredNumberScheduled
-                number_ready = status.numberReady
+                instance = sample.items[0]
+                status = instance.status
+
+                desired_number_scheduled = status.desiredNumberScheduled or 0
+                updated_number_scheduled = status.updatedNumberScheduled or 0
+                current_number_scheduled = status.currentNumberScheduled or 0
+                number_misscheduled = status.numberMisscheduled or 0
+                number_available = status.numberAvailable or 0
+                number_ready = status.numberReady or 0
+
                 if (
-                    desired_number_scheduled > 0
-                    and desired_number_scheduled == number_ready
+                    (scheduled and desired_number_scheduled)
+                    and desired_number_scheduled
+                    == updated_number_scheduled
+                    == current_number_scheduled
+                    == number_available
+                    == number_ready
+                    and number_misscheduled == 0
+                ) or not (
+                    scheduled
+                    or desired_number_scheduled
+                    or current_number_scheduled
+                    or number_available
+                    or number_ready
+                    or number_misscheduled
                 ):
                     return
 
